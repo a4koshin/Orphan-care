16c2c1121c58011bca7d247b5e02115cb83d2bed
a4koshin <mankajr11@gmail.com>
2026-03-17 18:17:06 +0300

    Hero image data is created

---
+++ lib/heroImages.ts
@@ -0,0 +1,27 @@
+interface HeroImg {
+    id:number,
+    image:string,
+}
+
+export const heroImg :HeroImg[] = [
+    {
+        id:0,
+        image:"/img1.jpg"
+    },
+    {
+        id:1,
+        image:"/img2.jpg"
+    },
+    {
+        id:2,
+        image:"/img3.jpg"
+    },
+    {
+        id:3,
+        image:"/img4.jpg"
+    },
+    {
+        id:4,
+        image:"/img5.jpg"
+    }
+]
