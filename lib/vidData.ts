a9fb929627eebc6588591c99b57418f29600a9eb
a4koshin <mankajr11@gmail.com>
2026-03-21 21:42:19 +0300

    Video data is created

---
+++ lib/vidData.ts
@@ -0,0 +1,27 @@
+interface VidoeData {
+  id: number;
+  title: string;
+  description: string;
+  videoUrl: string;
+}
+
+export const videoData: VidoeData[] = [
+  {
+    id: 0,
+    title: "Orphan Care Somalia 2019",
+    description: "Helping children in Somalia",
+    videoUrl: "https://youtu.be/ipuwGTm6fvg?si=Ki_n_CORdyZX8rYw",
+  },
+  {
+    id: 1,
+    title: "Orphan Care Somalia 2018",
+    description: "Helping children in Somalia",
+    videoUrl: "https://www.youtube.com/watch?v=TTisUWNr1kg",
+  },
+  {
+    id: 2,
+    title: "Orphan Care Somalia 2017",
+    description: "Helping children in Somalia",
+    videoUrl: "https://www.youtube.com/watch?v=uc-IydfXQ5g",
+  },
+];
