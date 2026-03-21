81a05d90580f23b92f8cdf361d7a36d5a0bae56d
a4koshin <mankajr11@gmail.com>
2026-03-21 19:44:41 +0300

    Pictures dummy data is being created to this file

---
+++ lib/picData.ts
@@ -0,0 +1,30 @@
+interface PicData {
+  id: number;
+  img: string;
+  title: string;
+  description: string;
+}
+
+export const picData: PicData[] = [
+  {
+    id: 0,
+    img: "/pic1.jpg",
+    title: "Orphan Care Association möte i staden Bo",
+    description:
+      "Borås (Orphancare.net) - En presentation på Corporate waxqagabadka Orphan Care föräldralösa barn på eftermiddagen och på kvällen före",
+  },
+  {
+    id: 1,
+    img: "/pic2.jpg",
+    title: "Konferens av Orphan jättestor i Göteborg",
+    description:
+      "Göteborg (Orphancare.net) - en av de årliga konvent för Association of Orphan Care hölls i Göteborg,",
+  },
+  {
+    id: 2,
+    img: "/pic3.jpg",
+    title: "Prestanda Orphan Care i Stockholm",
+    description:
+      "Göteborg (Orphancare.net) - 27 December 2014, myndigheterna i vården av föräldralösa Orphan Care Association har rest i Stockholm, Sveriges",
+  },
+];
