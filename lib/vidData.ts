46b52dc8d271ff33fe5f9268b05c4560a7c90c20
a4koshin <mankajr11@gmail.com>
2026-03-23 22:12:05 +0300

    Video Data is modified

--- lib/vidData.ts
+++ lib/vidData.ts
@@ -9,19 +9,19 @@ export const videoData: VidoeData[] = [
   {
     id: 0,
     title: "Orphan Care Somalia 2019",
-    description: "Helping children in Somalia",
+    description: "Hjälper barn i Somalia",
     videoUrl: "https://youtu.be/ipuwGTm6fvg?si=Ki_n_CORdyZX8rYw",
   },
   {
     id: 1,
     title: "Orphan Care Somalia 2018",
-    description: "Helping children in Somalia",
+    description: "Hjälper barn i Somalia",
     videoUrl: "https://www.youtube.com/watch?v=TTisUWNr1kg",
   },
   {
     id: 2,
     title: "Orphan Care Somalia 2017",
-    description: "Helping children in Somalia",
+    description: "Hjälper barn i Somalia",
     videoUrl: "https://www.youtube.com/watch?v=uc-IydfXQ5g",
   },
 ];
