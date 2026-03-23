b7b5f77d4d466db6d86c7d8e8c530b48c29a2382
a4koshin <mankajr11@gmail.com>
2026-03-23 22:15:45 +0300

    Video data is added

---
+++ lib/vidoer.ts
@@ -0,0 +1,105 @@
+interface VidoerData {
+  id: number;
+  title: string;
+  description: string;
+  videoUrl: string;
+}
+
+export const videorData: VidoerData[] = [
+  {
+    id: 0,
+    title: "Orphan Care Somalia 2019",
+    description: "Hjälper barn i Somalia",
+    videoUrl: "https://youtu.be/ipuwGTm6fvg?si=Ki_n_CORdyZX8rYw",
+  },
+  {
+    id: 1,
+    title: "Orphan Care Somalia 2018",
+    description: "Hjälper barn i Somalia",
+    videoUrl: "https://www.youtube.com/watch?v=TTisUWNr1kg",
+  },
+  {
+    id: 2,
+    title: "Orphan Care Somalia 2017",
+    description: "Hjälper barn i Somalia",
+    videoUrl: "https://www.youtube.com/watch?v=uc-IydfXQ5g",
+  },
+  {
+    id: 3,
+    title: "Ka farxinta ilmaha Agoonta 2024",
+    description: "Hjälper barn i Somalia",
+    videoUrl: "https://youtu.be/-xDsbfm22kQ?si=CVdP4t3_Un_Y2sUj",
+  },
+  {
+    id: 4,
+    title: "Orphan Care",
+    description: "Hjälper barn i Somalia",
+    videoUrl: "https://youtu.be/g9DTvpmYLl0?si=8FM9pM3CWurkUrm8",
+  },
+  {
+    id: 5,
+    title: "Orphan Care 2018",
+    description: "Hjälper barn i Somalia",
+    videoUrl: "https://youtu.be/65Kzj_qog6M?si=n3ipev1r-mfbA_H2",
+  },
+  {
+    id: 6,
+    title: "Orphan Care 2016",
+    description: "Hjälper barn i Somalia",
+    videoUrl: "https://youtu.be/CbMa_9_Kkvg?si=KNaLQ0RTM4JtcLKW",
+  },
+  {
+    id: 7,
+    title: "Orphan Care 2016",
+    description: "Hjälper barn i Somalia",
+    videoUrl: "https://youtu.be/1Lii-zJSQAY?si=iHO9VyDU1E__4bti",
+  },
+  {
+    id: 8,
+    title: "Orphan Care 2015",
+    description: "Hjälper barn i Somalia",
+    videoUrl: "https://youtu.be/TzpqX4NtYms?si=ldTfnsG2HAuxzQde",
+  },
+  {
+    id: 9,
+    title: "Orphan Care 2013",
+    description: "Hjälper barn i Somalia",
+    videoUrl: "https://youtu.be/IRuGv-9tSXU?si=SihgPCBnrRS0PkFC",
+  },
+  {
+    id: 10,
+    title: "Orphan Care 2012",
+    description: "Hjälper barn i Somalia",
+    videoUrl: "https://youtu.be/W9-GSKw7Fwg?si=Cq-xq_jS3ahIFnnJ",
+  },
+  {
+    id: 11,
+    title: "Orphan Care 2012",
+    description: "Hjälper barn i Somalia",
+    videoUrl: "https://youtu.be/gX0bGOR8a6g?si=XuXNAojqGbfhPhvF",
+  },
+  {
+    id: 12,
+    title: "Orphan Care 2012",
+    description: "Hjälper barn i Somalia",
+    videoUrl: "https://youtu.be/BkgHxQU880Q?si=amJafL8nTLaTBCw9",
+  },
+  {
+    id: 13,
+    title: "Orphan Care 2011",
+    description: "Hjälper barn i Somalia",
+    videoUrl: "https://youtu.be/EfUWvnOsXQ0?si=soPRMxBOhQaA2gI4",
+  },
+  {
+    id: 14,
+    title: "Orphan Care 2011",
+    description: "Hjälper barn i Somalia",
+    videoUrl: "https://youtu.be/Gmi5CxRFk00?si=WhiGmRTCXbN56dnP",
+  },
+  {
+    id: 15,
+    title: "Orphan Care 2011",
+    description: "Hjälper barn i Somalia",
+    videoUrl: "https://youtu.be/2DyzQ2-7_ys?si=YjYk9zAM2cJUsO-o",
+  },
+];
