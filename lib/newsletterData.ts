2fa401d652be078abafaba9af651ee89b40a2240
a4koshin <mankajr11@gmail.com>
2026-03-23 22:14:54 +0300

    Newsletter is added

---
+++ lib/newsletterData.ts
@@ -0,0 +1,36 @@
+export type NewsletterArticle = {
+  id: number;
+  image: string;
+  section: string;
+  title: string;
+  excerpt: string;
+};
+
+export const newsletterData: NewsletterArticle[] = [
+  {
+    id: 1,
+    image: "/new1.jpg",
+    section: "Ka farxinta ilmaha Agoonta 2024",
+    title: "Ka farxinta ilmaha Agoonta 2024",
+    excerpt:
+      "Munaasabad loogu tala galay ka farxinta ilmaha agoonta maalinta ciida ee nagu soo beegan oo aay Orphancare u qabatay maanta oo ku beegneed 07-april-2024 ayaa 153 arday lagu gudoonsiiyay min 50$ waxaana uga mahadcelineena Allahii inoo suurtagaliyay iyo bahda Orphan Care ku xiran ee ahlu kheyrka ah.",
+  },
+  {
+    id: 2,
+    image: "/new2.jpg",
+    section: "TILL ALLA BIDRAGSGIVARE!",
+    title: "TILL ALLA BIDRAGSGIVARE!",
+    excerpt:
+      "Som Ni er allihopa känner till, vi befinner oss fortfarande mitt i pandemi kulmen.För att undvika den höga smittorisken i år kommer vi inte har planerade möte, alltså orphancares årliga traditionen med anledning av pågående covid-19-kris så länge max 4 personer föreskrifter råder.Om Allaah vill (Inshaa'Allaah) åter samlar vi den dagen myndigheterna tillåter stora sammankomster.KU SOCOTA DEEQ BIXIYEYAASHA Sida aad dhamaantiinba la wada socotaan, waxaynu bartamaha kaga jirnaa halista caabuqa covid-19. Taxadar ahaan ayey bahda orphancare sanadkan u baajisay kulankii sanadlaha ee soo jireenka u ahaa, in alla inta uu taagan yahay qodobka kulamada ee ugu badnaan 4 qof inay markiiba kulmi karaan, si aynu u yareyno halista isqaadsiinta/ faafinta caabuqa covid-19.Haddii Allaha awooda leh ogolaado waxaynu dib u kulmi doonaa marka ay hay'adaha dowliga ahi qaadaan xayiraadaha kulamada noocan oo kale ah. Med vänliga hälsningar Salaan diiran oo idin kaga timid MAAMULKA ORPHANCARE",
+  },
+  {
+    id: 3,
+    image: "/news18.jpg",
+    section:
+      "Xaflad sanadeedkii 9-aad Orphan care ee 2019 oo ka dhacday magaalada Gothenburg ee Dalka Sweden",
+    title:
+      "Xaflad sanadeedkii 9-aad Orphan care ee 2019 oo ka dhacday magaalada Gothenburg ee Dalka Sweden",
+    excerpt:
+      "ALXAMDULILLAAH! Mahad idilkeed oo dhan Allaah ayaa iska leh oo inoo suuragaliyay in uu noo qabsoomo shir sanadeedkii 9aad ee ORPHANCARE. Waxaa mahadaas mid lamid ah leh walaalihii soo ajiibay codsigeena nagalana soo qeyb galay kulanka. Maamulka Orphancare ayaa soo bandhigay waxqabadkii iyo xisaab celintii 2019 taas oo ayna aad ula dhaceen marti sharaftii & muxsiniinta ku tabarucda dhaqaalaha waxbarasho/ka farxinta agoonta.Maamulka Orphancare ayaa halkaa ka sheegay in hiigsiga 2020 inuu yahay tiro kordhin lagu samayn doono agoonta wax barata, lana gaarsiin doono tirada 170 agoon oo dhigata dugsiyada kala duwan. Sidoo kale 35 agoon oo dhamaysay dugsiyada sare iyo Qur'aanka kariimka, wadarta guud ee agoonta Orphancare taakulaysay ayaa noqoneysa 205 agoon.HAL KU DHEGA ORPHANCARE AYAA LAGA SOO BILAABO TAN & AASAASKEEDII MAY 12, 2010 AHAA NAFTAADA KA QAAD oo NAFTAADA U QAAD. Maamulka Orphancare wuxuu mahad ballaaran u celinayaa dhamaan walaalaha sida joogtada ah uga qayb qaata taakuleynta iyo dib u dhiska agoonta Orphancare.",
+  },
+];
